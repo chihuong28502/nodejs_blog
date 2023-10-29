f506dababe761c3dcbeec4e97f1436bbb5af45bd
Chí Hưởng <>
2023-10-29 22:53:43 +0700

    push

--- src/index.js
+++ src/index.js
@@ -1,30 +1,33 @@
-const express = require("express");
-const path = require("path");
-const morgan = require("morgan");
-const exphbs = require("express-handlebars");
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
 const app = express();
 const port = 3002;
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static(path.join(__dirname, "public")));
+const route = require('./routes/index');
 
-app.use(morgan("combined"));
+app.use(
+                                              express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
+// app.use(morgan("combined"));
 
 // cấu hình handlebars
 app.engine(
-  "hbs",
-  exphbs.engine({
-    extname: ".hbs",
-  })
+    'hbs',
+    exphbs.engine({
+        extname: '.hbs',
+    }),
 );
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "resources/views"));
-app.get("/", (req, res) => {
-  res.render("home");
-});
-app.get("/news", (req, res) => {
-  res.render("news");
-});
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
+// routes init
+route(app);
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
